fe59121c17914c36df1fdbdf76776b323eee6c27
Bruno Oliveira <brunohosilva97@gmail.com>
2025-05-18 15:20:34 -0300

    created empty state to show to user

--- agenda/View/MySchedulesViewController.swift
+++ agenda/View/MySchedulesViewController.swift
@@ -29,6 +29,14 @@ class MySchedulesViewController: UIViewController, UITableViewDelegate {
     private let tableView = UITableView()
     private let viewModel = ScheduleViewModel()
     
+    //--------------------------------------------------------
+    // MARK: - UI Empty View State
+    //--------------------------------------------------------
+    
+    private let emptyStateView = UIView()
+    private let emptyLabel = UILabel()
+    private let emptyAddButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -77,6 +85,36 @@ class MySchedulesViewController: UIViewController, UITableViewDelegate {
         tableView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
+        
+        view.addSubview(emptyStateView)
+        
+        [emptyLabel, emptyAddButton].forEach {
+            emptyStateView.addSubview($0)
+        }
+        
+        emptyStateView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        emptyLabel.text = "Você não possui agendamentos."
+        emptyLabel.textAlignment = .center
+        emptyLabel.font = .systemFont(ofSize: 18)
+        emptyLabel.numberOfLines = 0
+
+        emptyAddButton.setTitle("Adicionar agendamento", for: .normal)
+        emptyAddButton.setTitleColor(.systemBlue, for: .normal)
+
+        emptyLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-20)
+            $0.left.right.equalToSuperview().inset(32)
+        }
+
+        emptyAddButton.snp.makeConstraints {
+            $0.top.equalTo(emptyLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
     }
     
     private func bind() {
@@ -85,11 +123,19 @@ class MySchedulesViewController: UIViewController, UITableViewDelegate {
             .bind(to: addButtonBinder)
             .disposed(by: disposeBag)
         
+        emptyAddButton.rx.tap
+            .bind(to: addButtonBinder)
+            .disposed(by: disposeBag)
+        
         viewModel.scheduleItemsObservable
-            .do(onNext: { print("atualizou->", $0) })
             .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { row, scheduleData, cell in
                 cell.textLabel?.text = scheduleData.title
             }
+            .disposed(by: disposeBag)
+        
+        viewModel.scheduleItemsObservable
+            .map { $0.isEmpty }
+            .bind(to: emptyStateBinder)
             .disposed(by: disposeBag)
         
         detailTapRelay
@@ -103,6 +149,13 @@ class MySchedulesViewController: UIViewController, UITableViewDelegate {
         deleteTapRelay
             .bind(to: deleteTapBinder)
             .disposed(by: disposeBag)
+    }
+    
+    private var emptyStateBinder: Binder<Bool> {
+        Binder(self) { target, isEmpty in
+            target.emptyStateView.isHidden = !isEmpty
+            target.tableView.isHidden = isEmpty
+        }
     }
     
     private var detailTapBinder: Binder<ScheduleModel> {
